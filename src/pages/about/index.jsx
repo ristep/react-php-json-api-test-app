fd7847ab851ec8d0664a884d64c6ec0375ef9f1a
ristep <ristepan@gmail.com>
2021-08-08 17:52:40 +0200

    merge userhook

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,44 +1,48 @@
 import React, { useState, useEffect } from "react";
-import toc from 'remark-toc';
-import Markdown from 'react-markdown';
+import toc from "remark-toc";
+import Markdown from "react-markdown";
 
 import { Card } from "react-bootstrap";
+import { useAuthData } from "hooks/authData";
+
 import ReactJson from "react-json-view";
 
-const AboutPage = (props) =>{
-  const { userData } = props;
-	const [markdown, setMarkdown] = useState('');
-  const [url] = useState("https://raw.githubusercontent.com/ristep/php-json-api-test-app/master/README.md");
+const AboutPage = () => {
+  const { loginState } = useAuthData();
+
+  const [markdown, setMarkdown] = useState("");
+  const [url] = useState(
+    "https://raw.githubusercontent.com/ristep/php-json-api-test-app/master/README.md"
+  );
 
   useEffect(() => {
-      fetch(url)
-        .then(function (response) {
-          if(response.ok){
-            return response.text();
-          }
-          throw new Error('Error message.');
-        })
-        .then(function (data) {
-          setMarkdown( data );
-        })
-        .catch(function (err) {
-      });
-  }, [url])
+    fetch(url)
+      .then(function (response) {
+        if (response.ok) {
+          return response.text();
+        }
+        throw new Error("Error message.");
+      })
+      .then(function (data) {
+        setMarkdown(data);
+      })
+      .catch(function (err) {});
+  }, [url]);
 
-	return(
-		<div>
-        <Card className="markdown-body mainMargin">
-          <Markdown
-            className="result"
-            source={markdown}
-            skipHtml={false}
-            escapeHtml={false}
-            plugins={[toc]}
-          />
-        </Card>
-        <ReactJson src={userData} />
-		</div> 
-  	)
-}
+  return (
+    <div>
+      <Card className="markdown-body mainMargin">
+        <Markdown
+          className="result"
+          source={markdown}
+          skipHtml={false}
+          escapeHtml={false}
+          plugins={[toc]}
+        />
+      </Card>
+      <ReactJson src={loginState} />
+    </div>
+  );
+};
 
 export default AboutPage;
--- src/pages/loginForm/index.jsx
+++ src/pages/loginForm/index.jsx
@@ -1,133 +1,136 @@
 // import "./form.scss";
 import React, { useState } from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  FormLabel,
+  Row,
+} from "react-bootstrap";
 import useValiHook from "../../hooks/formValidation";
-import Axios from "Axios";
 
 import * as yup from "yup";
 import ReactJson from "react-json-view";
 
 import "./index.scss";
+import { useAuthData } from "hooks/authData";
 
 let valSchema = yup.object().shape({
   username: yup.string().required(),
   password: yup.string().required(),
 });
 
-const getUserTokenQuery = (username, password) => ({
-  getToken: {
-    username: username,
-    password: password,
-  },
-});
-
-const LoginForm = (props) => {
-  const {loginState, setLoginState} = props.userModel;
+const LoginForm = () => {
   const [formData, setFormData] = useState({ username: "", password: "" });
-
-  const { onBlur, validate, errors } = useValiHook({ valSchema, formData });
-
-  const getKey = (e) => {
-    e.preventDefault();
-    (async () => {
-      setLoginState({});
-      await Axios.post(
-        "",
-        getUserTokenQuery(formData.username, formData.password)
-      )
-        .then((ret) => {
-          setLoginState({
-            ...ret.data,
-            status: ret.status,
-            statusText: ret.statusText,
-            error: false,
-          });
-        })
-        .catch((err) => {
-          setLoginState({
-            data:{},
-            status: 204,
-            statusText: "Data base error!!",
-            error: true,
-            errorNo: err.error,
-            errMessage: err.message,
-            name: err.name,
-            config: err.config,
-          });
-        });
-    })();
-    //    setUserdata(getUserTokenQuery(username, password));
-  };
+  const { onBlur, errors } = useValiHook({ valSchema, formData });
+  const { loginState, getKey, clearKey } = useAuthData();
 
   const onChange = (ev) => {
     setFormData({ ...formData, [ev.target.name]: ev.target.value });
   };
 
   return (
-    <Container id="login-box" >
-      <Card>
-        <Card.Title>
-          <Row>
-            <Col className="md-12">
-              <div id="login-box-title">User login</div>
-            </Col>
-          </Row>
-        </Card.Title>
-        <Card.Body>
-          <Form className="p-2 bg-light" size="sm">
-            <Form.Group>
-              <Form.Label>User name:</Form.Label>
-              <Form.Control
-                size="sm"
-                name="username"
-                type="text"
-                placeholder="User name"
-                value={formData?.username}
-                onChange={onChange}
-                onBlur={onBlur}
-                className={errors?.username ? "is-invalid" : ""}
-              />
-              <Form.Label className="invalid-feedback">
-                {errors?.username}
-              </Form.Label>
-            </Form.Group>
+    <>
+      {!loginState.OK && (
+        <Container id="login-box">
+          <Card>
+            <Card.Title>
+              <Row>
+                <Col className="md-12">
+                  <div id="login-box-title">User login</div>
+                </Col>
+              </Row>
+            </Card.Title>
+            <Card.Body>
+              <Form className="p-2 bg-light" size="sm" autocomplete="off">
+                <Form.Group>
+                  <Form.Label>User name:</Form.Label>
+                  <Form.Control
+                    size="sm"
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="User name"
+                    value={formData?.username}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    className={errors?.username ? "is-invalid" : ""}
+                  />
+                  <Form.Label className="invalid-feedback">
+                    {errors?.username}
+                  </Form.Label>
+                </Form.Group>
 
-            <Form.Group>
-              <Form.Label>Password:</Form.Label>
-              <Form.Control
-                size="sm"
-                name="password"
-                type="text"
-                placeholder="Password"
-                value={formData?.password || ""}
-                onChange={onChange}
-                onBlur={onBlur}
-                className={errors?.password ? "is-invalid" : ""}
-              />
-              <Form.Label className="invalid-feedback">
-                {errors?.password}
-              </Form.Label>
-            </Form.Group>
-          </Form>
-        </Card.Body>
+                <Form.Group>
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control
+                    size="sm"
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    value={formData?.password || ""}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    className={errors?.password ? "is-invalid" : ""}
+                  />
+                  <Form.Label className="invalid-feedback">
+                    {errors?.password}
+                  </Form.Label>
+                </Form.Group>
+              </Form>
+              {loginState.error && (
+                <div className="alert alert-danger" role="alert">
+                  {loginState.message}
+                </div>
+              )}
+            </Card.Body>
 
-        <Card.Footer>
-          <Row>
-            <Col className="md-4">
-            </Col>  
-            <Col className="md-4">
-              <Button variant="primary" onClick={getKey} id="submitButton">
-                Submit
-              </Button>
-            </Col>
-            <Col className="md-4">
-            </Col>  
-          </Row>
-        </Card.Footer>
-      </Card>
+            <Card.Footer>
+              <Row>
+                <Col className="md-4"></Col>
+                <Col className="md-4">
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={(ev) => getKey(ev, formData)}
+                    id="submitButton"
+                  >
+                    LOGIN
+                  </Button>
+                </Col>
+                <Col className="md-4">
+                <Button
+                    variant="standard"
+                    type="submit"
+                    onClick={(ev) => getKey(ev, formData)}
+                    id="submitButton"
+                  >
+                    NEW USER
+                  </Button>
 
-      <ReactJson src={loginState} />
-    </Container>
+                </Col>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </Container>
+      )}
+      {loginState.OK && (
+        <Container id="login-box">
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={() => clearKey()}
+            id="submitButton"
+          >
+            Logonut
+          </Button>
+          <ReactJson src={loginState} />
+        </Container>
+      )}
+    </>
   );
 };
 
